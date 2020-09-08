d7b5c9db40a3f39cf2a1e4c68a051e9e8bdf5962
alesamv <disasterology03@hotmail.com>
2020-09-07 19:52:48 -0500

    Update derecha.ino

--- derecha.ino
+++ derecha.ino
@@ -5,53 +5,48 @@ int tiempoRet = 1000;
 int estePin;
 int cuenta_der = 0;
 boolean bot_der = LOW;
+
 // Función setup
-void setup ()
-{
-pinMode (bot,INPUT);
-for (estePin = 0; estePin < numLeds; estePin++)
-{
-pinMode (led[estePin],OUTPUT);
+void setup () {
+  pinMode (bot,INPUT);
+  for (estePin = 0; estePin < numLeds; estePin++) {
+    pinMode (led[estePin],OUTPUT);
+  }
 }
-}
+
 // Función loop
-void loop ()
-{
-bot_der = digitalRead (bot);
-if (bot_der ==HIGH)
-{
-switch (cuenta_der)
-{
-case 0:
-digitalWrite(led[0],HIGH);
-break;
-case 1:
-digitalWrite (led[1],HIGH);
-digitalWrite (led[2],HIGH);
-digitalWrite (led[3],HIGH);
-break;
-case 2:
-digitalWrite (led[4],HIGH);
-digitalWrite (led[5],HIGH);
-digitalWrite (led[6],HIGH);
-break;
-case 3:
-digitalWrite (led[7],HIGH);
-digitalWrite (led[8],HIGH);
-digitalWrite (led[9],HIGH);
-break;
-default:
-;
-}
-delay (tiempoRet);
-cuenta_der++;
-if (cuenta_der > 3)
-{
-cuenta_der = 0;
-}
-}
-for (estePin = 0; estePin < numLeds; estePin++)
-{
-digitalWrite (led[estePin],LOW);
-}
+void loop () {
+  bot_der = digitalRead (bot);
+  if (bot_der ==HIGH) {
+    switch (cuenta_der) {
+      case 0:
+      digitalWrite(led[0],HIGH);
+      break;
+      case 1:
+      digitalWrite (led[1],HIGH);
+      digitalWrite (led[2],HIGH);
+      digitalWrite (led[3],HIGH);
+      break;
+      case 2:
+      digitalWrite (led[4],HIGH);
+      digitalWrite (led[5],HIGH);
+      digitalWrite (led[6],HIGH);
+      break;
+      case 3:
+      digitalWrite (led[7],HIGH);
+      digitalWrite (led[8],HIGH);
+      digitalWrite (led[9],HIGH);
+      break;
+      default:
+      ;
+    }
+    delay (tiempoRet);
+    cuenta_der++;
+    if (cuenta_der > 3) {
+      cuenta_der = 0;
+    }
+  }
+  for (estePin = 0; estePin < numLeds; estePin++) {
+    digitalWrite (led[estePin],LOW);
+  }
 }
