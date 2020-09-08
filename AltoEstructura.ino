6c9e11fe032ccf511731c09da406efbf142a573a
alesamv <disasterology03@hotmail.com>
2020-09-08 09:01:34 -0500

    Add files via upload

---
+++ AltoEstructura.ino
@@ -0,0 +1,37 @@
+/* Programa AltoEstructuras.ino 
+   Realiza el encendido de los diez leds de la rutina Alto,    y en cuyo código se incluye el uso de estructuras    de programación                                         */ 
+// Variables 
+int bot = 2; 
+int led[] = {4,5,6,7,8,9,10,11,12,13}; 
+int numLeds = 10; int tiempoRet = 400; int estePin; 
+boolean bot_alto = LOW; 
+ 
+// Función setup 
+void setup () 
+{
+  pinMode (bot,INPUT); 
+  for (estePin = 0; estePin < numLeds; estePin++) 
+  { 
+    pinMode (led[estePin],OUTPUT); 
+  } 
+}
+ 
+// Función loop 
+void loop () 
+{ 
+  bot_alto = digitalRead (bot);   
+  if (bot_alto==HIGH) 
+  { 
+    for (estePin = 0; estePin < numLeds; estePin++) 
+    { 
+      digitalWrite (led[estePin],HIGH); 
+    } 
+    delay (tiempoRet); 
+    for (estePin = 0; estePin < numLeds; estePin++) 
+    { 
+      digitalWrite (led[estePin],LOW); 
+    } 
+    delay (tiempoRet);  
+  } 
+} 
+
---
+++ DerechaEstructura.ino
@@ -0,0 +1,69 @@
+/* Programa DerechaEstructuras.ino 
+   Realiza el encendido de los diez leds de la rutina Derecha,    y en cuyo código se incluye el uso de estructuras    de programación                                         */ 
+// Variables 
+int bot = 2; 
+int led[] = {4,5,6,7,8,9,10,11,12,13}; 
+int numLeds = 10; 
+int tiempoRet = 400; 
+int estePin; 
+int cuenta_der = 0; 
+boolean bot_der = LOW; 
+ 
+// Función setup 
+void setup () 
+{ 
+  pinMode (bot,INPUT); 
+  for (estePin = 0; estePin < numLeds; estePin++) 
+  { 
+    pinMode (led[estePin],OUTPUT); 
+  } 
+} 
+
+// Función loop 
+void loop () 
+{ 
+  bot_der = digitalRead (bot);   
+  if (bot_der  ==HIGH) 
+  { 
+    switch (cuenta_der) 
+    {       
+      case 0:          
+      digitalWrite (led[0],HIGH);         
+      break;   
+          
+        case 1:         
+        digitalWrite (led[1],HIGH);         
+        digitalWrite (led[2],HIGH);         
+        digitalWrite (led[3],HIGH); 
+        break;       
+        
+        case 2:         
+        digitalWrite (led[4],HIGH);         
+        digitalWrite (led[5],HIGH); 
+        digitalWrite (led[6],HIGH); 
+        break;   
+
+        case 3:
+        digitalWrite (led[7],HIGH);
+        digitalWrite (led[8],HIGH);
+        digitalWrite (led[9],HIGH);
+        break; 
+        
+        default: 
+        ;    
+        } 
+    delay (tiempoRet);     
+    cuenta_der++;     
+    if (cuenta_der > 3) 
+    { 
+      cuenta_der = 0; 
+    }   
+    } 
+  for (estePin = 0; estePin < numLeds; estePin++) 
+  { 
+    digitalWrite (led[estePin],LOW); 
+  } 
+} 
+
+
+
